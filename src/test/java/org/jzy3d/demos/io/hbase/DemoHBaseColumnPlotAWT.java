ea43bb27fa53420448c756aada0b7745deb9cd09
Martin Pernollet <mpernollet@octo.com>
2015-04-12 14:06:18 +0200

    HBaseIO read rows

--- src/test/java/org/jzy3d/demos/io/hbase/DemoHBaseColumnPlotAWT.java
+++ src/test/java/org/jzy3d/demos/io/hbase/DemoHBaseColumnPlotAWT.java
@@ -20,52 +20,52 @@ import org.jzy3d.utils.LoggerUtils;
  */
 public class DemoHBaseColumnPlotAWT {
     public static int MILION = 1000000;
-
-    static String TABLE = DemoHBaseColumnPlotAWT.class.getSimpleName();
-    static String FAMILY = "demo";
+    public static String TABLE = DemoHBaseColumnPlotAWT.class.getSimpleName();
+    public static String FAMILY = "demo";
     static {
         LoggerUtils.minimal();
     }
 
     public static void main(String[] args) throws Exception {
-
+        
         int nRaws = 100; //MILION / 1000;
         int nPivotTheme = 8;
         int nPivotCol = 35 * nPivotTheme;
         int nCpCcCat = 12;
         int nCpCcCol = 10;
 
+        // Generate table data
         GeneratorKeyValue generator = new GeneratorKeyValue();
         final List<List<KeyVal<String, Float>>> rows = generator.vip(nRaws, nPivotCol, nCpCcCat, nCpCcCol);
 
+        // show generated table
         DrawableVBO drawable = new DrawableVBO(new VBOBuilderColumnDatabase(rows));
-        BigPicture.chartBlack(drawable, BigPicture.Type.ddd);
+        BigPicture.chart(drawable, BigPicture.Type.ddd).black();
 
-        
+        // dump in HBase table
         String[] families = { FAMILY };
-
         HBaseIO hbase = new HBaseIO();
         hbase.tableDelete(TABLE);
         hbase.tableCreate(TABLE, families);
+        hbase.putAll(rows, TABLE, FAMILY, progress());
+        //hbase.scanPrint(TABLE);
         
+        // clear memory, read what we previously dumped
+        rows.clear();
+        hbase.scanRows(TABLE, rows);
+        
+        // draw read data
+        DrawableVBO drawable2 = new DrawableVBO(new VBOBuilderColumnDatabase(rows));
+        BigPicture.chart(drawable2, BigPicture.Type.ddd).black();
+    }
+
+    private static Progress progress() {
         Progress progress = new Progress(){
             public void progress(int value) {
                 if(value%1000==0)
                     System.out.println(value + " inserted");
             }
         };
-        hbase.putAll(rows, TABLE, FAMILY, progress);
-
-        hbase.scanPrint(TABLE);
-        
-        
-        rows.clear();
-        hbase.scanRows(TABLE, rows);
-        
-        
-        
-        
-        DrawableVBO drawable2 = new DrawableVBO(new VBOBuilderColumnDatabase(rows));
-        BigPicture.chartBlack(drawable2, BigPicture.Type.ddd);
+        return progress;
     }
 }
